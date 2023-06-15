96333ddecca59791dbf0caa22b5d94dfe7f68d79
Haungeli101 <134581343+Haungeli101@users.noreply.github.com>
2023-06-15 14:41:13 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Elizabeth" />
+        <Header title="E" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
